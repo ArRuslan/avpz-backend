4244b39bb74b64125761161dca4d2fdd5737bf14
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-10-11 15:23:58 +0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 POETRY_VENV="$(poetry env info -p)"
 export PATH="${PATH}:${POETRY_VENV}/bin"
 
-#poetry run gunicorn ... --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --preload
+poetry run gunicorn PROJECT.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --preload
